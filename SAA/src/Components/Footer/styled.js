7f3a165cfe9e946162988915a07148103a4924cf
luanasmr <luanarodriguessmr@gmail.com>
2021-08-21 21:28:55 -0300

    Atualizando Footer

--- SAA/src/Components/Footer/styled.js
+++ SAA/src/Components/Footer/styled.js
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 
 
 export const AreaFooter = styled.div`
-    background-color:#ccc;
+    background-color:#a9a9a9;
     text-align: center;
     padding:20px;
     font-size: 14px;
-
+    color: white;
 
     bottom:0;
     position:fixed;
-    width:100%:
+    width:100%;
 
 
 `;
