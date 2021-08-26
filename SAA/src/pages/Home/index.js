4ffbece54d8b5d5e3aa392ae588b3e823ca76db3
CeciliaMarques <cecilia.marques.m.m@gmail.com>
2021-08-25 23:03:12 -0300

    teste

--- SAA/src/pages/Home/index.js
+++ SAA/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
 
 import { ContainerPage, TitlePage} from '../../Components/Main';
@@ -14,4 +14,30 @@ const Page = () => {
     );
 }
 
-export default Page;
+export default Page;*/
+import React, { useRef } from "react";
+import Demo from "./styled.jsx";
+
+const Map = () => {
+    const innerRef = useRef();
+
+    const getLocation = () => {
+        innerRef.current && innerRef.current.getLocation();
+    };
+
+    return (
+        <article style={{ textAlign: "center" }}>
+            {/* eslint-disable-next-line no-console*/}
+            <Demo onError={(error) => console.log(error)} ref={innerRef} />
+            <button
+                className="pure-button pure-button-primary"
+                onClick={getLocation}
+                type="button"
+            >
+                Get location
+            </button>
+        </article>
+    );
+};
+
+export default Map;
