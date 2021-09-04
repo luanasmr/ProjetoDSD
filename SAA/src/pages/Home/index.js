dd0fcdcc48dcfb0c039ed6ad7c9d1b820e8f75ce
CeciliaMarques <cecilia.marques.m.m@gmail.com>
2021-09-04 01:54:11 -0300

    testM

--- SAA/src/pages/Home/index.js
+++ SAA/src/pages/Home/index.js
@@ -1,11 +1,7 @@
 import React from "react";
-import Styled from 'components/Home/styled.js';
+import mapa from 'components/pages/Home/styled.js';
 const Map = () => {
-  const innerRef = useRef();
-
-  const getLocation = () => {
-      innerRef.current && innerRef.current.getLocation();
-  };
+ 
 
   return (
      <mapa/>
