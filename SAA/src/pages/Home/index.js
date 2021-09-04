9e666644c06217631733735b85307d6c88e09971
CeciliaMarques <cecilia.marques.m.m@gmail.com>
2021-09-04 01:09:21 -0300

    atualizacaoVializuacaoMapa

--- SAA/src/pages/Home/index.js
+++ SAA/src/pages/Home/index.js
@@ -1,4 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+export default () => {
+
+return(
 <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -9,4 +12,6 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
   </Marker>
-</MapContainer>
+</MapContainer>);
+
+}
