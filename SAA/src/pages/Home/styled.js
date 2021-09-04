1177fdbfe116d5c7b084db496c4cd01ba2766110
CeciliaMarques <cecilia.marques.m.m@gmail.com>
2021-09-04 20:28:05 -0300

    atualizacao

--- SAA/src/pages/Home/styled.js
+++ SAA/src/pages/Home/styled.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import{ MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-function mapa() {
+import React, { useState, useEffect } from "react";
+import { Map, TileLayer } from "react-leaflet";
+//import data from "../assets/data.json";
+//import Markers from "./VenueMarkers";
 
-return(
-<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>);
+import { useLocation, useHistory } from "react-router-dom";
 
-}
-export default mapa;
+import "leaflet/dist/leaflet.css";
+
+const MapView = (props) => {
+  const [state, setState] = useState({
+    currentLocation: { lat: 52.52437, lng: 13.41053 },
+    zoom: 13,
+    data,
+  });
+
+  const location = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (location.state.latitude && location.state.longitude) {
+      const currentLocation = {
+        lat: location.state.latitude,
+        lng: location.state.longitude,
+      };
+      console.log(state);
+      setState({
+        ...state,
+        data: {
+          venues: state.data.venues.concat({
+            name: "new",
+            geometry: [currentLocation.lat, currentLocation.lng],
+          }),
+        },
+        currentLocation,
+      });
+      history.replace({
+        pathname: "/map",
+        state: {},
+      });
+    }
+  }, [location]);
+
+  return (
+    <Map center={state.currentLocation} zoom={state.zoom}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Markers venues={state.data.venues} />
+    </Map>
+  );
+};
+
+export default MapView;
