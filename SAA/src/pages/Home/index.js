32d6c822250744834f05525b1f6d1f4db1d2cf78
CeciliaMarques <cecilia.marques.m.m@gmail.com>
2021-09-04 01:36:45 -0300

    at

--- SAA/src/pages/Home/index.js
+++ SAA/src/pages/Home/index.js
@@ -1,17 +1,15 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-export default () => {
+import React from "react";
+import Styled from 'components/Home/styled.js';
+const Map = () => {
+  const innerRef = useRef();
 
-return(
-<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>);
+  const getLocation = () => {
+      innerRef.current && innerRef.current.getLocation();
+  };
 
-}
+  return (
+     <mapa/>
+  );
+};
+
+export default Map;
