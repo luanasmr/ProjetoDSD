5e460a9f1dd00bf99dbb92dee85fc4ef9e8ed5e5
CeciliaMarques <cecilia.marques.m.m@gmail.com>
2021-09-14 23:11:29 -0300

    Obtendo Localização do Usuário

--- SAA/src/pages/Home/index.js
+++ SAA/src/pages/Home/index.js
@@ -13,13 +13,19 @@ export class MapContainer extends Component {
 
     this.state = {
       stores: [
-        { latitude:  -6.47511, longitude:  -35.4286},
-        { latitude: -26.9605363, longitude: -52.5335505, local: "Xaxim" },]
+        // { latitude:  -6.47511, longitude:  -35.4286},
+        // { latitude: -26.9605363, longitude: -52.5335505, local: "Xaxim" },
+      ]
     }
+    navigator.geolocation.getCurrentPosition( (pos) => {
+      this.setState({stores: [
+        {latitude: pos.coords.latitude, longitude: pos.coords.longitude}
+      ]});
+    });
   }
 
   displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
+    this.state.stores.map((store, index) => {
       return <Marker key={index} id={index} position={{
         lat: store.latitude,
         lng: store.longitude
@@ -49,4 +55,3 @@ export default GoogleApiWrapper(
     apiKey: 'AIzaSyDT-VIUQmnhKmkLF2ylVGfJlw9Sa9xoN64',
   }
   ))(MapContainer)
-
