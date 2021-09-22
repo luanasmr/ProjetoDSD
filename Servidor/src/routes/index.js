b19444ff0ef8aad12ff0ccebdea99d83443338c7
CeciliaMarques <cecilia.marques.m.m@gmail.com>
2021-09-22 11:24:02 -0300

    rotas funcionando

--- Servidor/src/routes/index.js
+++ Servidor/src/routes/index.js
@@ -1,10 +1,17 @@
 const {Router} = require('express');
 const router = Router();
 
-
-const {getUsers, createUser} = require('../controles/indexcontroles.js');
+const { getUsers, getUserById, createUser, updateUser, deleteUser, createHist, deleteHist, getHist,getHistById,updateHist  } = require('../controles/indexcontroles.js');
 
 router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
 router.post('/users', createUser);
+router.put('/users/:id', updateUser)
+router.delete('/users/:id', deleteUser);
+router.get('/historico', getHist);
+router.get('/historico/:idhist', getHistById);
+router.post('/historico', createHist);
+router.put('/historico/:idhist', updateHist)
+router.delete('/historico/:idhist', deleteHist);
 
 module.exports = router;
