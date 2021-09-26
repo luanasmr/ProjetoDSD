fc8cc211f3f3944c9387581a999caca615546535
CeciliaMarques <cecilia.marques.m.m@gmail.com>
2021-09-25 23:21:23 -0300

    problema do cors resolvido

--- Servidor/src/index.js
+++ Servidor/src/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 
 
 //milldewares
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use((req, res, next)=>{
+    res.header("Access-control-Allow-Origin", "*")
+    app.use(cors());
+    next();
+
+});
 
 //routes
 app.use(require('./routes/index'));
 
 
-app.listen(3000);
-console.log('Servidor na porta:', 3000);
+app.listen(4000);
+console.log('Servidor na porta:', 4000);
+
+
